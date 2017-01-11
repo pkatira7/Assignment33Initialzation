ffd51e532b5840c2b16f5c9f53559d97cc9ab9fa
pkatira7 <pkatira@vmware.com>
2017-01-11 14:34:32 +0530

    Add files via upload

---
+++ Init.java
@@ -0,0 +1,11 @@
+package assignment33Initialization;
+
+public class Init {
+	
+		 int x; int y;
+		 String name;
+		 public static void main(String args[]) {
+		 Init pnt = new Init();
+		 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+		} }
+
